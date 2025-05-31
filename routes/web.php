b6bb004df78f488c005ba6f7146eeb4929ff931b
severusDude <fajarsaladin05@gmail.com>
2025-05-31 19:15:59 +0700

    kontl12

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
